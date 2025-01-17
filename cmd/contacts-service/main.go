ccbee19e02e7d9ad9cbba0ebfe79e26767efb831
Pawel Szewczuk <pawel.szewczuk.h@gmail.com>
2025-01-17 14:08:09 +0100

    Added contacts service endpoints for adding, removing and checking contact status

--- cmd/contacts-service/main.go
+++ cmd/contacts-service/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/Horqu/zkp-communicator-backend/internal/contacts"
+	"github.com/Horqu/zkp-communicator-backend/internal/db"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	// Connect to the database
+	conn, err := db.ConnectGORM()
+	if err != nil {
+		log.Fatal("DB connection failed: ", err)
+	}
+
+	// Auto-migrate the database
+	if err := db.AutoMigrateAll(conn); err != nil {
+		log.Fatal("DB migration failed: ", err)
+	}
+
+	log.Default().Println("DB connection successful")
 
 	// Ping endpoint
 	r.GET("/ping", func(c *gin.Context) {
@@ -17,14 +32,14 @@ func main() {
 		})
 	})
 
-	// Dodawanie kontaktu
-	r.POST("/contacts", contacts.AddContactHandler)
+	// AddContactHandler creates a new contact
+	r.POST("/contacts", contacts.AddContactHandler(conn))
 
-	// Usuwanie kontaktu
-	r.DELETE("/contacts/:contactID", contacts.RemoveContactHandler)
+	// RemoveContactHandler deletes an existing contact
+	r.DELETE("/contacts/:contactID", contacts.RemoveContactHandler(conn))
 
-	// Weryfikacja statusu kontaktu
-	r.GET("/contacts/:contactID/status", contacts.VerifyContactStatusHandler)
+	// VerifyContactStatusHandler gets the status of an existing contact
+	r.GET("/contacts/:contactID/status", contacts.VerifyContactStatusHandler(conn))
 
 	r.Run(":8084")
 }
