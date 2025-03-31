07c1b7f1c8f58411c45ac8fd897147a30a6fd2d0
Pawel Szewczuk <pawel.szewczuk.h@gmail.com>
2025-03-31 19:08:16 +0200

    :bento: ZKP Sigma server site finished

--- cmd/encryption/zkp_challenge.go
+++ cmd/encryption/zkp_challenge.go
@@ -6,35 +6,57 @@ import (
 	"log"
 	"math/big"
 
-	db "github.com/Horqu/zkp-communicator-backend/cmd/database"
-	"github.com/Horqu/zkp-communicator-backend/cmd/database/queries"
-
 	"github.com/consensys/gnark-crypto/ecc/bn254"
 )
 
-func GenerateZkSnarkProof(username string) bn254.G1Affine {
+func GenerateSigmaChallenge() (*big.Int, *big.Int) {
 
-	publicKey, err := queries.GetPublicKeyByUsername(db.GetDBInstance(), username)
+	r, err := rand.Int(rand.Reader, bn254.ID.ScalarField())
 	if err != nil {
-		log.Println(err)
+		panic(err)
 	}
 
-	challenge, err := StringToG1Affine(publicKey)
+	var g bn254.G1Affine
+	g.X.SetString("1")
+	g.Y.SetString("2")
+
+	var t bn254.G1Affine
+	t.ScalarMultiplication(&g, r)
+
+	e, err := rand.Int(rand.Reader, bn254.ID.ScalarField())
 	if err != nil {
-		log.Println(err)
+		panic(err)
 	}
 
-	return challenge
+	return e, r // save e, t | send e, r
 }
 
-func VerifyZkSnarkProof(check bn254.G1Affine, y bn254.G1Affine) bool {
+func GenerateSigmaProof(privateKey *big.Int, e *big.Int, r *big.Int) *big.Int {
 
-	if check.Equal(&y) {
-		return true
-	} else {
-		return false
-	}
+	s := new(big.Int).Mul(e, privateKey)
+	s.Add(s, r)
+	s.Mod(s, bn254.ID.ScalarField())
 
+	return s // received e, r | send s
+}
+
+func VerifySigmaProof(t *bn254.G1Affine, e *big.Int, s *big.Int, publicKey bn254.G1Affine) bool {
+
+	var g bn254.G1Affine
+	g.X.SetString("1")
+	g.Y.SetString("2")
+
+	// Obliczenie g^s
+	var gS bn254.G1Affine
+	gS.ScalarMultiplication(&g, s)
+
+	// Obliczenie t * publicKey^e
+	var yE, tPlusYE bn254.G1Affine
+	yE.ScalarMultiplication(&publicKey, e)
+	tPlusYE.Add(t, &yE)
+
+	// Sprawdzenie, czy g^s == t * publicKey^e
+	return gS.Equal(&tPlusYE) // read e, t | received s
 }
 
 func GenerateSchnorrChallenge(publicKey string) (*big.Int, *big.Int) {
@@ -62,10 +84,10 @@ func GenerateSchnorrChallenge(publicKey string) (*big.Int, *big.Int) {
 	e := new(big.Int).SetBytes(h.Sum(nil))
 	e.Mod(e, bn254.ID.ScalarField()) // e needs to be stored
 
-	return e, r
+	return e, r // send e, r
 }
 
-func GenerateSchnorrProof(publicKey string, privateKey *big.Int, e *big.Int, r *big.Int) *big.Int {
+func GenerateSchnorrProof(privateKey *big.Int, e *big.Int, r *big.Int) *big.Int {
 
 	s := new(big.Int).Mul(e, privateKey)
 	s.Add(s, r)
@@ -117,7 +139,7 @@ func GenerateFeigeFiatShamirChallenge() (*big.Int, *big.Int) {
 	}
 	log.Println("Wyzwanie e (1024 bity):", e.Text(2)) // Wyświetlenie w postaci binarnej
 
-	return N, e
+	return N, e // save and send N, e
 }
 
 func GenerateFeigeFiatShamirProof(privateKey *big.Int, N *big.Int, e *big.Int) ([]*big.Int, []*big.Int, *big.Int) {
@@ -185,94 +207,4 @@ func VerifyFeigeFiatShamir(xList []*big.Int, yList []*big.Int, v *big.Int, N *bi
 		}
 	}
 	return true
-}
-
-func Schnorr_proof() {
-	// Generator na krzywej BN254
-	var g bn254.G1Affine
-	g.X.SetString("1")
-	g.Y.SetString("2")
-
-	// Generowanie klucza prywatnego x
-	x, err := rand.Int(rand.Reader, bn254.ID.ScalarField())
-	log.Println("Klucz prywatny x:", x)
-	if err != nil {
-		panic(err)
-	}
-
-	// Obliczenie klucza publicznego y = g^x
-	var y bn254.G1Affine
-	y.ScalarMultiplication(&g, x)
-	log.Println("Klucz publiczny y:", y.String())
-
-	// Wygenerowanie losowej wartości r
-	r, err := rand.Int(rand.Reader, bn254.ID.ScalarField())
-	if err != nil {
-		panic(err)
-	}
-
-	// Obliczenie zobowiązania R = g^r
-	var R bn254.G1Affine
-	R.ScalarMultiplication(&g, r)
-
-	// Obliczenie wyzwania e = H(R || y)
-	h := sha256.New()
-	h.Write(R.Marshal())
-	h.Write(y.Marshal())
-	e := new(big.Int).SetBytes(h.Sum(nil))
-	e.Mod(e, bn254.ID.ScalarField())
-
-	// Obliczenie odpowiedzi s = r + e * x
-	// Klient potrzebuje e, r (wszystko od serwera)
-	s := new(big.Int).Mul(e, x)
-	s.Add(s, r)
-	s.Mod(s, bn254.ID.ScalarField())
-
-	// Weryfikacja dowodu: sprawdzamy, czy g^s = R + y^e
-	// Serwer potrzebuje g, y, R, e, s (s od klienta)
-	var gS, yE, RplusYE bn254.G1Affine
-	gS.ScalarMultiplication(&g, s)
-	yE.ScalarMultiplication(&y, e)
-	RplusYE.Add(&R, &yE)
-
-	if gS.Equal(&RplusYE) {
-		log.Println("Dowód Schnorra poprawny: klient zna klucz prywatny.")
-	} else {
-		log.Println("Dowód Schnorra niepoprawny: klient nie zna klucza.")
-	}
-}
-
-func Gnark_crypto_main() {
-	// Generator punktu na krzywej BN254
-	var g bn254.G1Affine
-	g.X.SetString("1") // Wartość generatora X
-	g.Y.SetString("2") // Wartość generatora Y
-
-	if !g.IsOnCurve() {
-		panic("Punkt nie należy do krzywej")
-	}
-	// Wygenerowanie sekretnego klucza x
-	// x, err := rand.Int(rand.Reader, fr.Modulus())
-	x, err := rand.Int(rand.Reader, bn254.ID.ScalarField())
-	if err != nil {
-		panic(err)
-	}
-
-	log.Println("Sekretny klucz x:", x)
-
-	// Obliczenie y = g^x
-	var y bn254.G1Affine
-	y.ScalarMultiplication(&g, x)
-
-	log.Println("Publiczny klucz y:", y.String())
-
-	// Weryfikacja dowodu (sprawdzenie, czy x rzeczywiście generuje y)
-	var check bn254.G1Affine
-	check.ScalarMultiplication(&g, x) // to musi zwrocic klient do serwera
-
-	if check.Equal(&y) {
-		log.Println("Dowód poprawny: posiadacz zna x")
-	} else {
-		log.Println("Dowód błędny: x nie pasuje")
-	}
 }
--- cmd/encryption/zkp_challenge_test.go
+++ cmd/encryption/zkp_challenge_test.go
@@ -27,7 +27,7 @@ func TestSchnorrProof(t *testing.T) {
 
 	// Generowanie wyzwania i zobowiązania
 	e, r := GenerateSchnorrChallenge(yString)
-	s := GenerateSchnorrProof(yString, x, e, r)
+	s := GenerateSchnorrProof(x, e, r)
 
 	// Weryfikacja dowodu
 	var R bn254.G1Affine
@@ -59,7 +59,7 @@ func TestSchnorrProofFailure(t *testing.T) {
 
 	// Generowanie wyzwania i zobowiązania
 	e, r := GenerateSchnorrChallenge(yString)
-	s := GenerateSchnorrProof(yString, x, e, r)
+	s := GenerateSchnorrProof(x, e, r)
 
 	// Celowe wprowadzenie błędu: zmodyfikowanie klucza publicznego
 	var yModified bn254.G1Affine
@@ -98,4 +98,77 @@ func TestFeigeFiatShamirProof(t *testing.T) {
 		t.Errorf("Dowód Feige-Fiat-Shamir nie został zweryfikowany poprawnie")
 	}
 
+}
+
+func TestSigmaProof(test *testing.T) {
+	// Generator na krzywej BN254
+	var g bn254.G1Affine
+	g.X.SetString("1")
+	g.Y.SetString("2")
+
+	// Generowanie klucza prywatnego
+	privateKey, err := rand.Int(rand.Reader, bn254.ID.ScalarField())
+	if err != nil {
+		test.Fatalf("Błąd generowania klucza prywatnego: %v", err)
+	}
+
+	// Obliczenie klucza publicznego publicKey = g^privateKey
+	var publicKey bn254.G1Affine
+	publicKey.ScalarMultiplication(&g, privateKey)
+
+	// Generowanie wyzwania i zobowiązania
+	e, r := GenerateSigmaChallenge()
+
+	// Obliczenie zobowiązania t = g^r
+	var t bn254.G1Affine
+	t.ScalarMultiplication(&g, r)
+
+	// Generowanie dowodu
+	s := GenerateSigmaProof(privateKey, e, r)
+
+	// Weryfikacja dowodu
+	if VerifySigmaProof(&t, e, s, publicKey) {
+		test.Log("Dowód Sigma Protocol zweryfikowany poprawnie")
+	} else {
+		test.Errorf("Dowód Sigma Protocol nie został zweryfikowany poprawnie")
+	}
+}
+
+func TestSigmaProofFailure(test *testing.T) {
+	// Generator na krzywej BN254
+	var g bn254.G1Affine
+	g.X.SetString("1")
+	g.Y.SetString("2")
+
+	// Generowanie klucza prywatnego
+	privateKey, err := rand.Int(rand.Reader, bn254.ID.ScalarField())
+	if err != nil {
+		test.Fatalf("Błąd generowania klucza prywatnego: %v", err)
+	}
+
+	// Obliczenie klucza publicznego publicKey = g^privateKey
+	var publicKey bn254.G1Affine
+	publicKey.ScalarMultiplication(&g, privateKey)
+
+	// Generowanie wyzwania i zobowiązania
+	e, r := GenerateSigmaChallenge()
+
+	// Obliczenie zobowiązania t = g^r
+	var t bn254.G1Affine
+	t.ScalarMultiplication(&g, r)
+
+	// Generowanie dowodu
+	s := GenerateSigmaProof(privateKey, e, r)
+
+	// Celowe wprowadzenie błędu: zmodyfikowanie klucza publicznego
+	var modifiedPublicKey bn254.G1Affine
+	modifiedPublicKey.X.SetString("123456789") // Nieprawidłowy punkt na krzywej
+	modifiedPublicKey.Y.SetString("987654321")
+
+	// Weryfikacja dowodu z błędnym kluczem publicznym
+	if VerifySigmaProof(&t, e, s, modifiedPublicKey) {
+		test.Errorf("Dowód Sigma Protocol został zweryfikowany poprawnie, mimo że powinien zakończyć się niepowodzeniem")
+	} else {
+		test.Log("Dowód Sigma Protocol poprawnie nie został zweryfikowany (test zakończony niepowodzeniem zgodnie z oczekiwaniami)")
+	}
 }
